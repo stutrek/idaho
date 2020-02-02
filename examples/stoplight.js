635ac8f82b02d623101eb9e4df80483f29200800
Stu Kabakoff <sakabako@gmail.com>
2020-02-02 17:52:24 -0500

    fix example, remove old ones

--- examples/stoplight.js
+++ examples/stoplight.js
@@ -1,145 +1,91 @@
-const { State, Machine } = require('../dist/index.js');
+const { useEffect, useStateData, Machine } = require('../dist/index');
 
-// a standard street light
-const Green = (exports.Green = class Green extends State {
-    light = 'green';
-});
+/*
+ 
+This example shows how to use Idaho to create a stoplight.
 
-const Yellow = (exports.Yellow = class Yellow extends State {
-    light = 'yellow';
-});
+Each state has a boolean for red, yellow, and green. Each state transitions to the next state after a timer.
 
-const Red = (exports.Red = class Red extends State {
-    light = 'red';
-});
+*/
 
-class Off extends State {
-    light = undefined;
-}
-
-class EastWest extends State {
-    effects = [
-        () => {
-            this.machine.parent.north.transition(Red);
-            this.machine.parent.south.transition(Red);
-            this.machine.parent.east.transition(Green);
-            this.machine.parent.west.transition(Green);
-
-            let timeout = setTimeout(() => {
-                this.machine.parent.east.transition(Yellow);
-                this.machine.parent.west.transition(Yellow);
-
-                timeout = setTimeout(() => {
-                    // transition machine to the other direction
-                    this.machine.transition(NorthSouth);
-                }, 5000);
-            }, 25000);
-
-            return () => {
-                clearTimeout(timeout);
-            };
-        },
-    ];
-}
-
-class NorthSouth extends State {
-    effects = [
-        () => {
-            this.machine.parent.north.transition(Green);
-            this.machine.parent.south.transition(Green);
-            this.machine.parent.east.transition(Red);
-            this.machine.parent.west.transition(Red);
-
-            let timeout = setTimeout(() => {
-                this.machine.parent.north.transition(Yellow);
-                this.machine.parent.south.transition(Yellow);
-
-                timeout = setTimeout(() => {
-                    // transition machine to the other direction
-                    this.machine.transition(EastWest);
-                }, 5000);
-            }, 25000);
-
-            return () => {
-                clearTimeout(timeout);
-            };
-        },
-    ];
-}
-
-class NormalOperation extends State {
-    static useHistory = true;
-    timer = new Machine(NorthSouth, this.machine); // second argument sets the parent machine
-}
-
-class BlinkingRed extends State {
-    data = {
-        on: true,
-    };
-
-    effects = [
-        () => {
-            const interval = setInterval(() => {
-                const NextState = this.data.on ? Off : Red;
-                this.machine.north.transition(NextState);
-                this.machine.south.transition(NextState);
-                this.machine.east.transition(NextState);
-                this.machine.west.transition(NextState);
-                this.setData({
-                    on: !this.data.on,
-                });
-            }, 3000);
-
-            return () => {
-                clearInterval(interval);
-            };
-        },
-    ];
-}
-
-class Emergency extends State {
-    constructor(machine, direction) {
-        super(machine);
-        this.emergencyDirection = direction;
-    }
-    effects = [
-        () => {
-            this.machine.north.transition(Red);
-            this.machine.south.transition(Red);
-            this.machine.east.transition(Red);
-            this.machine.west.transition(Red);
-            if (this.machine[this.emergencyDirection]) {
-                this.machine[this.emergencyDirection].transition(Green);
-            }
-        },
-    ];
-}
-
-class LightTriplet extends Machine {
-    constructor(initialState, parentMachine, direction) {
-        super(initialState, parentMachine);
-        this.direction = direction;
-    }
-}
-
-// this is how a street light normally works
-exports.StreetLightSystem = class StreetLightSystem extends Machine {
-    static initialState = BlinkingRed;
-
-    north = new LightTriplet(Green, this, 'north'); // passing in this allows it to trigger child events
-    south = new LightTriplet(Green, this, 'south');
-    east = new LightTriplet(Red, this, 'east');
-    west = new LightTriplet(Red, this, 'west');
-
-    powerOutage = () => {
-        this.transition(BlinkingRed);
-    };
-
-    resumeNormalOperation = () => {
-        this.transition(NormalOperation);
-    };
-
-    receiveEmergency = direction => {
-        this.transition(Emergency, direction);
+const red = control => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            control.transition('green');
+        }, 30000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    });
+    return {
+        red: true,
+        yellow: false,
+        green: false,
     };
 };
+
+const yellow = control => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            control.transition('red');
+        }, 3000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    });
+    return {
+        red: false,
+        yellow: true,
+        green: false,
+    };
+};
+
+const green = control => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            control.transition('yellow');
+        }, 27000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    });
+    return {
+        red: false,
+        yellow: false,
+        green: true,
+    };
+};
+
+const blinkingRed = control => {
+    const [isOn, setIsOn] = useStateData(true);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsOn(!isOn);
+        }, 3000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [isOn]);
+    return {
+        red: isOn,
+        yellow: false,
+        green: false,
+    };
+};
+
+const states = {
+    red,
+    green,
+    yellow,
+    blinkingRed,
+};
+
+const machine = new Machine(states, 'blinkingRed');
+
+setTimeout(() => {
+    machine.transition('red');
+}, 20000);
+
+machine.on('change', machine => {
+    console.log('change!', machine.stateName, machine.state);
+});
